64c2105a3e2be9161af825c0936138ffef4aa2d4
AmrikTheProgrammer <amrik.ahluwalia@gmail.com>
2020-04-06 15:55:05 +0100

    Done

--- Person.java
+++ Person.java
@@ -1,13 +1,19 @@
 package elevatorSimulation;
 
+import java.util.Random;
+
 public class Person {
 	
 	String name;
-	int floor;
+	int currentFloor;
+	int spaceRequired;
+	String direction;
+	int whatFloor; // to store what floor the guy wants to go to
 	
-	public Person (String Name) {
-		this.name = Name;
-		floor = 0;
+	public Person (String name, int space) {
+		this.name = name;
+		currentFloor = 0;
+		spaceRequired = space;
 	}
 	
 	public String returnName() {
@@ -15,14 +21,29 @@ public class Person {
 	}
 	
 	public void buttonPress() {
+		String floor = Integer.toBinaryString(currentFloor);
+		System.out.println("The Lift was called at floor"+floor);
 		
 	}
 	
 	public int returnFloor() {
-		return floor;
+		return currentFloor;
 	}
 	
-	public int chooseFloor() {
+	public void chooseFloor(int numberOfFloors) {
 		//generate random number for floor to go to
+		Random R = new Random();
+		whatFloor = R.nextInt((numberOfFloors - 1) + 1) + 1;
+	}
+	
+	public void setFloor(Elevator E) {
+		if (whatFloor == E.currentFloor) {
+			currentFloor = whatFloor;
+		}
+		
+	}
+	
+	public int getSpace() {
+		return spaceRequired;
 	}
 }
