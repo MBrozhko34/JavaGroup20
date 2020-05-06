f2ab9ebec3580ac9fdadd744b9c911093a0a4919
SaharBatol <sahar.batol@yahoo.co.uk>
2020-05-06 16:06:54 +0100

    Created study class

--- Main/courseWork/Simulation.java
+++ Main/courseWork/Simulation.java
@@ -14,11 +14,14 @@ public class Simulation {
 	Clients c1;
 	int complaints=0;
 	Random randomness;
+	Study studyTest;
+	
 
 
 	public Simulation(MenuController m) {
 		MenuVars = m;
 		tick = 0;
+		
 
 	}
 	
@@ -53,6 +56,45 @@ public class Simulation {
 		}
 		System.out.println("The average waiting time is: "+totalAverageTime);
 		System.out.println("The total number of complaints: "+complaints);
+		
+		//if(complaints>5) {
+		//	System.out.println("We have had more than 5 complaints, this is bad");
+		//}
+	}
+	
+	
+	public void runStudy() {
+		Building building1 = new Building(MenuVars,this);
+		randomness = new Random();
+		randomness.setSeed(MenuVars.getSeed());
+		ArrivalSimulator s = new ArrivalSimulator(randomness);
+		PeopleCreator p = new PeopleCreator(MenuVars, building1,s,randomness);
+		//Ui = new TextBasedUI(building1,this);
+		//Ui.showUI();
+		
+		for (int i=0; i< MenuVars.getTicks();i++) {
+			building1.tick();
+			p.tick();
+			//Ui.showUI();
+			tick++;
+		}
+		for(Person p1: building1.allPeople) {
+			//int waitingTime = 0;
+			//waitingTime=p.getInLift-p.startWaiting;
+			if(p1.getInLift!=0) {
+				allWaiting=allWaiting+(p1.getInLift-p1.startWaiting);
+				totalAverageTime=allWaiting/building1.allPeople.size();
+			}
+			if(p1.name=="Client") {
+				if((p1.getInLift-p1.startWaiting)>1) {
+					complaints++;
+				}
+			}			
+		}
+		System.out.println("The average waiting time is: "+totalAverageTime);
+		System.out.println("The total number of complaints: "+complaints);
+		System.out.println("The probability of P is: "+s.getPProbability()); 
+		System.out.println("The probability of Q is: "+s.getQProbability());
 		
 		//if(complaints>5) {
 		//	System.out.println("We have had more than 5 complaints, this is bad");
--- Main/courseWork/Study.java
+++ Main/courseWork/Study.java
@@ -1,9 +1,22 @@
 package courseWork;
 
 public class Study {
+	ArrivalSimulator aSimu; 
+	Simulation simu; 
 
-	public Study() {
-		// TODO Auto-generated constructor stub
+	public Study(Simulation studyS) { 
+		simu=studyS;
+	}
+	
+	public void testStudy() {
+		for (int i=0; i<5; i++) {
+			aSimu.getPProbability(); 
+			aSimu.getQProbability();
+			simu.runStudy(); 
+		}
+		
+		
+		
 	}
 
 }
